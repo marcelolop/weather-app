ed95996f3dac53f94a2c6a46a636be58b4d4d415
Marcelolop <marcelolopfern@hotmail.com>
2023-11-11 17:56:37 -0600

    adding polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -21,4 +21,11 @@ module.exports = {
   plugins: [
     new Dotenv(),
   ],
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify")
+    }
+  }
 };
